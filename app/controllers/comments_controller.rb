d3d467ac512a1f706b054c060806192078d121f4
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-02 17:56:27 +0000

    Save

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,20 @@
 class CommentsController < ApplicationController
+  
   def create
-    comment = Comment.new
-    comment.author_id = params.fetch("query_author_id")
-    comment.venue_id = params.fetch("query_venue_id")
-    comment.body = params.fetch("query_body")
-    comment.save
+    input_venue_id = params.fetch("query_venue_id")
+    input_author_id = params.fetch("query_author_id")
+    input_comment = params.fetch("query_body"_
     
-    redirect_to("/venues/#{comment.venue_id}")
+    new_comment = Comment.new
+    new_comment.venue_id = input_venue_id
+    new_comment.author_id = input_author_id
+    new_comment.body = input_body
+    new_comment.save
+    
+    #matching_photos = Photo.where(id: input_photo_id)
+    #the_photo = matching_photos.first
+
+    next_url = "/venues/" + the_comment.venue_id.to_s
+    redirect_to(next_url)
   end
 end
