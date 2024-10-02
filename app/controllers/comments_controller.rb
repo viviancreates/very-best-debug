e5b8320b8d41722feed5233ddb534a88592d6256
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-02 21:55:05 +0000

    Start comment over, route works

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,26 @@
 class CommentsController < ApplicationController
   
   def create
-    input_venue_id = params.fetch("query_venue_id")
-    input_author_id = params.fetch("query_author_id")
-    input_comment = params.fetch("query_body"_
-    
-    new_comment = Comment.new
-    new_comment.venue_id = input_venue_id
-    new_comment.author_id = input_author_id
-    new_comment.body = input_body
-    new_comment.save
-    
-    #matching_photos = Photo.where(id: input_photo_id)
-    #the_photo = matching_photos.first
 
-    next_url = "/venues/" + the_comment.venue_id.to_s
-    redirect_to(next_url)
+    # /insert_comment_record?query_venue_id=11&query_author_id=wdew&query_body=wedewd" 
+    #input_venue_id = params.fetch("query_venue_id")
+    #input_author_id = params.fetch("query_author_id")
+    #input_comment = params.fetch("query_body")
+    
+    #new_comment = Comment.new
+    #new_comment.venue_id = input_venue_id
+   # new_comment.author_id = input_author_id
+    #new_comment.body = input_comment
+    #new_comment.save
+
+    #matching_venues = Venue.where(id => input_venue_id)
+    #venue = matching_comments.first
+
+  #next_url = "/photos/" + the_photo.id.to_s
+  #redirect_to(next_url)
+
+    #redirect_to("/venues/#{venue.id}")
+
+    render({ :template => "comment_templates/create"})
   end
 end
