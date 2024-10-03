af31b947396165e145accfd3b616b0774e2b6b32
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-03 00:57:07 +0000

    Add comments, relook at user update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,15 @@
 class CommentsController < ApplicationController
   
   def create
-
     # /insert_comment_record?query_venue_id=11&query_author_id=wdew&query_body=wedewd" 
-    #input_venue_id = params.fetch("query_venue_id")
-    #input_author_id = params.fetch("query_author_id")
-    #input_comment = params.fetch("query_body")
     
-    #new_comment = Comment.new
-    #new_comment.venue_id = input_venue_id
-   # new_comment.author_id = input_author_id
-    #new_comment.body = input_comment
-    #new_comment.save
+    new_comment = Comment.new
+    new_comment.venue_id = params.fetch("query_venue_id")
+    new_comment.author_id = params.fetch("query_author_id")
+    new_comment.body = params.fetch("query_body")
+    new_comment.save
 
-    #matching_venues = Venue.where(id => input_venue_id)
-    #venue = matching_comments.first
+    redirect_to("/venues/#{new_comment.venue_id}")
 
-  #next_url = "/photos/" + the_photo.id.to_s
-  #redirect_to(next_url)
-
-    #redirect_to("/venues/#{venue.id}")
-
-    render({ :template => "comment_templates/create"})
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,13 +28,8 @@ class UsersController < ApplicationController
   end
   
   def update
-    user_id = params.fetch("user_id")
-    matching_users = User.where({ :id => user_id })
-    the_user = matching_users.at(0)
     
-    the_user.username = params.fetch("query_username")
-    the_user.save
-    redirect_to("/users/#{user.username}")
+    
   end
 
 end
